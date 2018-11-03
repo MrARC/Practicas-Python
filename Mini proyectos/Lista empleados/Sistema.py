977f4c800a4b316a99bbe4a2ad51f50219d9d329
Alfonso Reyes <arc980103@gmail.com>
2018-11-03 15:07:16 -0700

    Lista de empleados terminada, con paginas

--- Mini proyectos/Lista empleados/Sistema.py
+++ Mini proyectos/Lista empleados/Sistema.py
@@ -3,6 +3,7 @@ import sys
 import csv
 from Empleado import Empleado
 
+
 class Sistema:
 
     # Constructor de instancia de clase
@@ -11,11 +12,11 @@ class Sistema:
         loading = True
         nDots = 0
         while loading:
-            # ya que la salida se guarda en el buffer la información que se va a 
+            # ya que la salida se guarda en el buffer la información que se va a
             # escribir se almacena antes de ser escrita a la terminal, con el parametro
             # flush podemos escribir todo lo del buffer a la terminal sin esperar
             nDots += 1
-            print('.', end='', flush=True)
+            print(".", end="", flush=True)
             time.sleep(0.02)
             if nDots >= 30:
                 loading = False
@@ -32,10 +33,12 @@ class Sistema:
             "0 - Salir",
             "/h",
         ]
+
     # Imprime el menu del sistema:
     @classmethod
     def imprimirMenu(self):
-        print('\n')
+        self.limpiarConsola()
+        print("\n")
         for elemento in self.obtenerElementosMenu():
             if elemento == "/h":
                 print("------------------------------")
@@ -47,30 +50,86 @@ class Sistema:
     @classmethod
     def getInput(self, msg):
         opcion = input(msg)
-        if opcion is not '':
-            self.handleInput(int(opcion))
+        if opcion is not "":
+            try:
+                self.handleInput(int(opcion))
+            except ValueError:
+                print('Opción no valida')
+                time.sleep(2)
+                
+    @classmethod
+    def limpiarConsola(self):
+        print("\n" * 100)
 
     @classmethod
     def obtenerEmpleados(self):
         with open("empleados.csv") as csvFile:
-            csvReader = csv.reader(csvFile, delimiter=',')
-            next(csvReader) # saltar el header
-            print('Nombre'.ljust(15), end='')
-            print('Apellido'.ljust(20), end='')
-            print('Puesto'.ljust(25), end='')
-            print('\n')
+            csvReader = csv.reader(csvFile, delimiter=",")
+            next(csvReader)  # saltar el header
+            # paginador
+            empleados = []
             for row in csvReader:
-                print(row[0].ljust(15) + row[1].ljust(20) + row[2].ljust(25))
-                print('\n')
-
+                empleados.append((row[0], row[1], row[2]))
+            def header():
+                print("\n----------------------------------")
+                print("Lista de empleados AURA")
+                print(f"Empleados registrados: {len(empleados)}")
+                print("----------------------------------")
+            # maxima cantidad de empleados por pag = 5
+            nPags = len(empleados) // 5
+            nPags = 1 if nPags == 0 else nPags
+            pagActual = 1
+            def lista():
+                header()
+                print("Pagina: {0}/{1}".format(pagActual, nPags))
+                print("Nombre".ljust(15), end="")
+                print("Apellido".ljust(20), end="")
+                print("Puesto".ljust(25))
+                print("----------------------------------")
+                elementos = pagActual * 5
+                for x in range(elementos-5, elementos):
+                    empleado = empleados[x]
+                    print(empleado[0].ljust(15) + empleado[1].ljust(20) + empleado[2].ljust(25))
+                print("----------------------------------")
+            def bottom():
+                texto_indicador = ""
+                if pagActual > 1 and pagActual != nPags:
+                    texto_indicador = "Pag anterior: A | Siguiente pag: S | Salir: 0"
+                elif pagActual == nPags:
+                    texto_indicador = "Pag anterior: A | Salir: 0"
+                else:
+                    texto_indicador = "Siguiente pag: S | Salir: 0"
+                print(texto_indicador)
+            while True:
+                self.limpiarConsola()
+                lista()
+                bottom()
+                sigOpcion = input("Selecciona una opción: ")
+                if sigOpcion == "A" or sigOpcion == 'a':
+                    if(pagActual >= 2):
+                        pagActual -= 1
+                    else:
+                        print('Opción no permitida')
+                        time.sleep(2)
+                elif sigOpcion == "S" or sigOpcion == 's':
+                    if(pagActual >= 1 and pagActual != nPags):
+                        pagActual += 1
+                    else:
+                        print('Opción no permitida')
+                        time.sleep(2)
+                elif sigOpcion == "0":
+                    break
+                else:
+                    print('Comando desconocido')
+                    time.sleep(2)
     @classmethod
     def añadirEmpleado(self):
         print("\nHas seleccionado añadir empleado")
         time.sleep(1)
         print("- Ahora introduce los datos del empleado -")
-        nombre = input('Nombre: ')
-        apellido = input('Apellido: ')
-        puesto = input('Puesto de trabajo: ')
+        nombre = input("Nombre: ")
+        apellido = input("Apellido: ")
+        puesto = input("Puesto de trabajo: ")
         empleado = Empleado(nombre, apellido)
         print(empleado.nombre)
 
@@ -82,6 +141,7 @@ class Sistema:
             self.obtenerEmpleados()
         if opcion == 0:
             exit()
+
 
 # Se executa si el código es el archivo principal
 if __name__ == "__main__":
